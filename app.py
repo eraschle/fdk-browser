287dcb389ed6532efb731636ee94d26e700b3245
Erich Raschle <erichraschle@gmail.com>
2023-06-05 22:32:05 +0200

    added streamlit agraph

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
+import streamlit
+from streamlit_agraph import agraph, Node, Edge, Config
 from pathlib import Path
 from tkinter import filedialog
 import tkinter as tk
-import os
-import calendar
-from datetime import datetime
 from typing import Callable, Iterable, List, Optional
 
 import streamlit as st
@@ -156,3 +155,34 @@ if selected == 'Visualization':
             # fig = go.Figure(data)
             # fig.update_layout(margin=dict(l=0, r=0, t=5, b=5))
             # st.plotly_chart(fig, use_container_width=True)
+            nodes = []
+            edges = []
+            nodes.append(Node(id="Spiderman",
+                              label="Peter Parker",
+                              size=25,
+                              shape="circularImage",
+                              image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_spiderman.png")
+                         )  # includes **kwargs
+            nodes.append(Node(id="Captain_Marvel",
+                              size=25,
+                              shape="circularImage",
+                              image="http://marvel-force-chart.surge.sh/marvel_force_chart_img/top_captainmarvel.png")
+                         )
+            edges.append(Edge(source="Captain_Marvel",
+                              label="friend_of",
+                              target="Spiderman",
+                              # **kwargs
+                              )
+                         )
+
+            config = Config(width=750,
+                            height=950,
+                            directed=True,
+                            physics=True,
+                            hierarchical=False,
+                            # **kwargs
+                            )
+
+            return_value = agraph(nodes=nodes,
+                                  edges=edges,
+                                  config=config)
